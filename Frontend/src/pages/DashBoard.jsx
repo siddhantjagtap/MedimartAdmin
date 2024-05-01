6c536c1f5ef8453a373f6cf89689f66b3ce6259c
siddhantjagtap <siddhant999123@gmail.com>
2024-05-01 17:21:58 +0530

    lelo

--- Frontend/src/pages/DashBoard.jsx
+++ Frontend/src/pages/DashBoard.jsx
@@ -66,7 +66,7 @@ const Dashboard = () => {
     return (
         <>
             <div className="flex flex-col min-h-screen  md:block hidden">
-                <header className="bg-gray-200 py-4 px-8 w-[100%] shadow flex justify-between">
+                <header className="bg-gray-200 py-4 px-8 shadow flex justify-between">
                     <h1 className="text-2xl ml-[1rem] font-bold">NEXIBLES</h1>
                     <div className='flex'>
                     <BsFillBellFill className='items-center pt-2 mr-[1rem] text-4xl'/>
