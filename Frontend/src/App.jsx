ec71188dfbc73e208d594f16dd90b9d9ff82ee0c
siddhantjagtap <siddhant999123@gmail.com>
2024-05-21 12:24:42 +0530

    siddhant

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,29 @@
-// import React ,{ useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import DashBoard from './Pages/DashBoard.jsx';
-// import OrderList from './Pages/OrderList';
-// import Payment from './Pages/Payment';
-// import Login from './Pages/Login.jsx';
-// import ManageProducts from './Pages/ManageProducts.jsx'
-// import OrderStatus from './Pages/OrderStatus.jsx'
+import React ,{ useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashBoard from './Pages/DashBoard.jsx';
+import OrderList from './Pages/OrderList';
+import Payment from './Pages/Payment';
+import Login from './Pages/Login.jsx';
+import ManageProducts from './Pages/ManageProducts.jsx'
+import OrderStatus from './Pages/OrderStatus.jsx'
 
-// function App() {
-//   const [totalOrders, setTotalOrders] = useState(0);
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/" element={<DashBoard totalOrders={totalOrders}/>} />
-//         <Route path="/orderlist" element={<OrderList totalOrders={totalOrders} setTotalOrders={setTotalOrders} />} />
-//         <Route path="/orderstatus" element={<OrderStatus />} />
-//         <Route path="/payment" element={<Payment />} />
-//         <Route path="/manageproducts" element={<ManageProducts />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  const [totalOrders, setTotalOrders] = useState(0);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DashBoard totalOrders={totalOrders}/>} />
+        <Route path="/orderlist" element={<OrderList totalOrders={totalOrders} setTotalOrders={setTotalOrders} />} />
+        <Route path="/orderstatus" element={<OrderStatus />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/manageproducts" element={<ManageProducts />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
-
-
+export default App;
 
 
-<h1>hiiiii</h1>
+
